cbdb06ee4b1eba19e3a8576fb922d8668b469687
naohide <57.x.mas@gmail.com>
2020-08-27 10:45:23 +0900

    Fix arg

--- main.py
+++ main.py
@@ -9,12 +9,23 @@ from utils import init_loggers, random_sleep
 
 logger = logging.getLogger('sns.main')
 
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
 if __name__ == '__main__':
     init_loggers()
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--sns', default='twitter')
-    parser.add_argument('--follow', default=True, type=bool)
+    parser.add_argument('--follow', default=True, type=str2bool)
     args = parser.parse_args()
 
     if args.sns == 'twitter':
